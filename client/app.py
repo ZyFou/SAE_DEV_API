1fdfc0eb065ae30694a28a0d9ba0f76d54aabba7
ZyFou <zyfodexe@gmail.com>
2024-03-18 18:50:50 +0100

    maj client

--- client/app.py
+++ client/app.py
@@ -4,29 +4,32 @@ from flask import Flask, render_template
 import requests
 app = Flask(__name__)
 
-POKEAPI_BASE_URL = "https://pokeapi.co/api/v2/"
-
-response = requests.get(POKEAPI_BASE_URL + "pokemon")
-
-nbPokemons = response.json().get("count")
-tabPokemonsAPI = response.json().get('results')
-
+base_url = "http://127.0.0.1:5000/api/"
 
 
 @app.route('/')
-def home():
-    return render_template('index.html', pokemons=tabPokemonsAPI, nb=nbPokemons)
+def index():
+    return render_template('index.html')
 
 
-def get_pokemon_details(name):
-    # Effectuer une requête pour obtenir les détails d'un Pokémon
-    response = requests.get(POKEAPI_BASE_URL + f'pokemon/{name}')
-    pokemon = response.json()
-    return pokemon
+@app.route('/<string:name>')
+def index_user(name):
+    request = requests.get(base_url + f"infos/{name}")
+    user = request.json()
+    return render_template('index.html', user=user)
 
 
-@app.route('/pokemon/<string:name>')
-def pokemon_detail(name):
-    pokemon = get_pokemon_details(name)
-    return render_template('detail.html', pokemon=pokemon)
+
+
+# def get_pokemon_details(name):
+#     # Effectuer une requête pour obtenir les détails d'un Pokémon
+#     response = requests.get(POKEAPI_BASE_URL + f'pokemon/{name}')
+#     pokemon = response.json()
+#     return pokemon
+
+
+# @app.route('/pokemon/<string:name>')
+# def pokemon_detail(name):
+#     pokemon = get_pokemon_details(name)
+#     return render_template('detail.html', pokemon=pokemon)
     
