5842330d97ee7c5c9686a890d1edf58794233e7f
ZyFou <zyfodexe@gmail.com>
2024-03-20 18:42:18 +0100

    FIX LOGIN

--- client/app.py
+++ client/app.py
@@ -1,15 +1,11 @@
 # flask run --port=8080 --debug
 
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, url_for
 import requests
 import mysql.connector
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="sae_api"
-)
+db_infos = {"host" : "localhost", "user":"root", "password":"", "database":"sae_api"}
+
 
 
 app = Flask(__name__)
@@ -24,10 +20,8 @@ temp_stockage = {"you" : {"Chracter": None}, "stats" : {"hp" : 0, "att": 0, "def
 
 @app.route('/')
 def index():
-    
     pfp = default_pfp
     if 'user_id' in session:
-        user_id = session['user_id']
         pfp = session["profile_picture"]
         
         return render_template('index.html', profile_picture=pfp, is_co = True)
@@ -37,47 +31,56 @@ def index():
 
 
 def verify_credentials(email, password):
-    try:
-        cursor = db.cursor()
-        cursor.execute("SELECT id, password, profile_picture, banner FROM users WHERE email = %s", (email,))
-        user = cursor.fetchone()
+    db = mysql.connector.connect(
+        host=db_infos['host'],
+        user=db_infos['user'],
+        password=db_infos['password'],
+        database=db_infos['database']
+    )
+    
+    cursor = db.cursor()
+    cursor.execute("SELECT id, password, profile_picture FROM users WHERE email = %s", (email,))
+    user = cursor.fetchone()
+
+    print("Mot de passe stocké dans la base de données:", user[1])
+    print("Mot de passe fourni par l'utilisateur:", password)
+
+    if user and password == user[1]:
         cursor.close()
         db.close()
-
-        if user and password == user[1]:
-            return {'id': user[0], 'profile_picture': user[2]}
-        else:
-            return None
-    except:
-        print("Erreur de connexion à la base de données:")
+        return {'id': user[0], 'profile_picture': user[2]}
+    else:
         return None
+ 
+
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
+    if 'user_id' in session:
+        return redirect('/')
+    else:
+        if request.method == 'POST':
+            email = request.form.get('email')
+            password = request.form.get('password')
 
-        if email and password:
-            user = verify_credentials(email, password)
-        
-            if user:
-                session['user_id'] = user['id']
-                session['profile_picture'] = user['profile_picture']
-                return redirect('/')
-        
-        # Si l'utilisateur n'est pas trouvé ou si les identifiants sont incorrects
-        return render_template('login.html', error="Identifiants incorrects. Veuillez réessayer.")
-    
-    # Rendre le modèle de connexion par défaut si aucune donnée de formulaire n'est envoyée
-    return render_template('login.html', error="")
-
+            if email and password:
+                user = verify_credentials(email, password)
+            
+                if user:
+                    session['user_id'] = user['id']
+                    session['profile_picture'] = user['profile_picture']
+                    return redirect('/')
+                else:
+                    return render_template('login.html', error="Identifiants incorrects. Veuillez réessayer.")
+            
+        else:
+            return render_template('login.html', error="")
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if 'user_id' in session:
+    if not 'user_id' in session:
         return render_template('/register.html')
     else:
         return redirect('/')
@@ -111,6 +114,7 @@ def profile():
 def logout():
     session.pop('user_id', None)
     session.pop('profile_picture', None)
+
     return redirect('/')
 
 
