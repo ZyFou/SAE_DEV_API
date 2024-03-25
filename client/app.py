1dfe802af7cc1213c3d359209557992569a053c0
ZyFou <zyfodexe@gmail.com>
2024-03-25 20:49:46 +0100

    Revenu en arriere session pick etc

--- client/app.py
+++ client/app.py
@@ -136,120 +136,30 @@ def gameMode_Custom():
     else:
         return redirect('/')
         
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
     
+
+# FAIRE LA SESSION
+
 @app.route('/gameMode/customGame')
 def testGame():
     if 'user_id' in session:
         query_params = request.args
+        print(session)
 
         required_params = ['yourPick', 'ennemy', 'stage']
         missing_params = [param for param in required_params if param not in query_params]
 
         if missing_params:
-            # Handle the case where required parameters are missing
-            return render_template('missing_params.html', missing_params=missing_params)
+            return redirect('/')
 
-        # Check if session variables are set
-        if "yourPick" not in session or "ennemy" not in session or "stage" not in session:
-            # Fetch data from API and set session variables
-            yourPick = requests.get(f"{base_url}characters/{query_params['yourPick']}").json()
-            ennemy = requests.get(f"{base_url}characters/{query_params['ennemy']}").json()
-            stage = requests.get(f"{base_url}stages/{query_params['stage']}").json()
+        yourPick = requests.get(f"{base_url}characters/{query_params['yourPick']}").json()
+        ennemy = requests.get(f"{base_url}characters/{query_params['ennemy']}").json()
+        stage = requests.get(f"{base_url}stages/{query_params['stage']}").json()
 
-            session["yourPick"] = yourPick
-            session["ennemy"] = ennemy
-            session["stage"] = stage
-
-        else:
-            if (session["yourPick"] != query_params['yourPick'] or 
-                session["ennemy"] != query_params['ennemy'] or 
-                session["stage"] != query_params['stage']):
-                
-                yourPick = requests.get(f"{base_url}characters/{query_params['yourPick']}").json()
-                ennemy = requests.get(f"{base_url}characters/{query_params['ennemy']}").json()
-                stage = requests.get(f"{base_url}stages/{query_params['stage']}").json()
-                
-                session["yourPick"] = yourPick
-                session["ennemy"] = ennemy
-                session["stage"] = stage
-                return render_template('game/modes/custom_game.html', yourPick=yourPick, ennemy=ennemy, stage=stage)
-
-
-        yourPick = session["yourPick"]
-        ennemy = session["ennemy"]
-        stage = session["stage"]
         return render_template('game/modes/custom_game.html', yourPick=yourPick, ennemy=ennemy, stage=stage)
 
     else:
         return redirect('/')
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
-# C'est cassé
 
 
 
